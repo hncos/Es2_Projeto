3ec4a4f7e045c3f8372431ba075504bfd60c90d2
hncos-iscteiul <hncos@iscte-iul.pt>
2020-03-26 17:13:16 +0000

    JavaDoc

--- Es2/src/main/java/MainProgram/Launcher.java
+++ Es2/src/main/java/MainProgram/Launcher.java
@@ -1,13 +1,23 @@
 package MainProgram;
-
+/**
+ * 
+ * @author HNCOS
+ *
+ */
 public class Launcher {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 
+	/**
+	 * 
+	 * @param a it's an integer
+	 * @param b it's an integer
+	 * @return the sum between the param a and param b
+	 */
 	public static int sum(int a, int b) {
 		return a + b;
 	}
-		//dedwu9eihi
+		
 }
